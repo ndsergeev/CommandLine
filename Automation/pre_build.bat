724c204891a2853db54f8b462361e92565004978
Nikita Sergeev <sergeevkep@icloud.com>
2024-10-08 23:13:57 +1100

    Updated MacOS and Win Scripts, Squash Commit:
    commit ef1520930341da31ff3d0ef60a5f421fc084e304
    Author: Nikita Sergeev <sergeevkep@icloud.com>
    Date:   Tue Oct 8 23:13:21 2024 +1100
    
        Fixed *.bat
    
    commit fa1d564c2c6c8bd479bd50f16a73734bb1414c37
    Author: Nikita Sergeev <sergeevkep@icloud.com>
    Date:   Mon Oct 7 19:31:30 2024 +1100
    
        Added support for both python and python3

--- Automation/pre_build.bat
+++ Automation/pre_build.bat
@@ -1,27 +1,32 @@
 @echo off
 
-set "script_path=%~f0"
 set "script_dir=%~dp0"
 
 echo //// //// //// //// //// //// //// //// //// //// //// ////
 echo //// //// //// //// Before Unreal Build //// //// //// ////
 echo //// //// //// //// //// //// //// //// //// //// //// ////
 
+if "%~1"=="" (
+    echo "Error: No path provided."
+    exit /b 1
+)
 set "exec_path=%~1"
+
 if not exist "%exec_path%" (
-    echo Error: The path '%exec_path%' does not exist.
+    echo "Error: The path '%exec_path%' does not exist."
     exit /b 1
 )
 
-where python3 >nul 2>&1
-if errorlevel 1 (
-    echo Error: Python 3 is not installed.
-    exit /b 2
-) else (
-    for /f "delims=" %%i in ('where python3') do set "python_path=%%i"
-    echo Python 3 is available at: %python_path%
+for %%I in (python python3) do (
+    where %%I >nul 2>nul
+    if not errorlevel 1 (
+        set "python_cmd=%%I"
+        goto :PythonFound
+    )
 )
 
+:PythonFound
+echo Python is available at: %python_cmd%
 echo Printing files from: %exec_path%
-python3 "%script_dir%walk_over_folders.py" "%exec_path%"
+%python_cmd% %script_dir%walk_over_folders.py %exec_path%
 echo //// //// //// //// //// //// //// //// //// //// //// ////
