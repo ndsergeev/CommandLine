ef1520930341da31ff3d0ef60a5f421fc084e304
Nikita Sergeev <sergeevkep@icloud.com>
2024-10-08 23:13:21 +1100

    Fixed *.bat

--- Automation/pre_build.bat
+++ Automation/pre_build.bat
@@ -1,32 +1,32 @@
 @echo off
 
-:: set "script_path=%~f0"
 set "script_dir=%~dp0"
 
 echo //// //// //// //// //// //// //// //// //// //// //// ////
 echo //// //// //// //// Before Unreal Build //// //// //// ////
 echo //// //// //// //// //// //// //// //// //// //// //// ////
 
+if "%~1"=="" (
+    echo "Error: No path provided."
+    exit /b 1
+)
 set "exec_path=%~1"
+
 if not exist "%exec_path%" (
-    echo Error: The path '%exec_path%' does not exist.
+    echo "Error: The path '%exec_path%' does not exist."
     exit /b 1
 )
 
-where python3 >nul 2>&1
-if not errorlevel 1 (
-    for /f "delims=" %%i in ('where python3') do set "python_cmd=%%i"
-) else (
-    where python >nul 2>&1
+for %%I in (python python3) do (
+    where %%I >nul 2>nul
     if not errorlevel 1 (
-        for /f "delims=" %%i in ('where python') do set "python_cmd=%%i"
-    ) else (
-        echo Error: Python is not installed.
-        exit /b 2
+        set "python_cmd=%%I"
+        goto :PythonFound
     )
 )
 
+:PythonFound
 echo Python is available at: %python_cmd%
 echo Printing files from: %exec_path%
-"%python_cmd%" "%script_dir%walk_over_folders.py" "%exec_path%"
+%python_cmd% %script_dir%walk_over_folders.py %exec_path%
 echo //// //// //// //// //// //// //// //// //// //// //// ////
